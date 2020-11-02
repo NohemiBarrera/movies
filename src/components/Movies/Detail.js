ffab78daae178af50edddc094740d83cbc166730
Meme <nohemi.baol@gmail.com>
2020-11-01 23:40:43 -0600

    corrige cardmedia de detalle

--- src/components/Movies/Detail.js
+++ src/components/Movies/Detail.js
@@ -17,21 +17,22 @@ const useStyles = makeStyles({
     paddingLeft: "1rem",
   },
   media: {
-    height: "auto",
-    paddingTop: "141.25%", // 16:9
-    backgroundSize: "contain",
-    width: "100%",
+    width: "50%",
   },
+  mediaContainer: {
+      display: "flex",
+      justifyContent: "center"
+  }
 });
 
 const Detail = (props) => {
   const classes = useStyles();
   const dispatch = useDispatch();
-  const overview = useSelector(state => state.movies.overview);
-  const vote_average = useSelector(state => state.movies.vote_average);
-  const release_date = useSelector(state => state.movies.release_date);
+  const overview = useSelector((state) => state.movies.overview);
+  const vote_average = useSelector((state) => state.movies.vote_average);
+  const release_date = useSelector((state) => state.movies.release_date);
   const title = useSelector((state) => state.movies.title);
-  const runtime = useSelector(state => state.movies.runtime);
+  const runtime = useSelector((state) => state.movies.runtime);
   const poster_path = useSelector((state) => state.movies.poster_path);
   console.log(poster_path);
 
@@ -45,32 +46,40 @@ const Detail = (props) => {
     <div className={classes.root}>
       <h2>Detalle</h2>
       <Grid container>
-        <Grid lg={6} xs={12}>
-          <Card className={classes.root}>
+        <Grid lg={6} xs={12} className={classes.mediaContainer}>
+          <Card className={classes.media}>
             <CardMedia
-              className={classes.media}
+              component="img"
+              alt={title}
+              height="auto"
               image={`https://image.tmdb.org/t/p/w500${poster_path}`}
               title={title}
             />
           </Card>
         </Grid>
-        <Grid lg={6} xs={12} >
+        <Grid lg={6} xs={12}>
           <h2 className={classes.info}>{title}</h2>
           <Grid container>
-              <Grid xs={6}>
-                <p>Fecha de estreno <b>{release_date}</b></p>
-              </Grid>
-              <Grid xs={6}>
-                <p>Duración <b>{runtime}</b></p>
-              </Grid>
+            <Grid xs={6}>
+              <p>
+                Fecha de estreno <b>{release_date}</b>
+              </p>
+            </Grid>
+            <Grid xs={6}>
+              <p>
+                Duración <b>{runtime}</b>
+              </p>
+            </Grid>
           </Grid>
           <Grid container>
-              <Grid xs={6}>
-                <p>Calificaión <b>{vote_average}</b></p>
-              </Grid>
-              <Grid xs={6}>
-                <p>Agregar a favoritos Icon</p>
-              </Grid>
+            <Grid xs={6}>
+              <p>
+                Calificaión <b>{vote_average}</b>
+              </p>
+            </Grid>
+            <Grid xs={6}>
+              <p>Agregar a favoritos Icon</p>
+            </Grid>
           </Grid>
           <h3>Resumen</h3>
           <p>{overview}</p>
